2b96342dadd4b95ad7979e4aec7df5643681445d
Hsin-Yu, Huang <r08922136@csie.ntu.edu.tw>
2022-05-27 17:27:49 +0800

    upload tusi motion

---
+++ Tusi motion/Tusi motion/Assets/Script/BallMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using TreeEditor;
+using UnityEditor;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float projectLineAngle;
+    Vector3 ballPosition;
+    public float counter;
+    private float ballSpeed = 0.01f;
+    public bool isShooting = false;
+    private float shootingSpeed = 50f;
+
+    public GameObject target;            // The position that that camera will be following.
+    public float smoothing = 5f;        // The speed with which the camera will be following.
+    public Vector3 ShootDirection;
+
+    void Update()
+    {
+        if (!isShooting)
+        {
+            TusiMotion();
+        }
+        else
+        {
+            transform.position += ShootDirection * shootingSpeed * Time.deltaTime;
+        }
+
+        if (Vector3.Distance(transform.position, target.transform.position) > 100f)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    void TusiMotion()
+    {
+        ballPosition.Set(2 * Mathf.Cos(projectLineAngle) * Mathf.Cos(counter - projectLineAngle), 0f,
+            2 * Mathf.Sin(projectLineAngle) * Mathf.Cos(counter - projectLineAngle));
+        transform.position = target.transform.position + ballPosition;
+        counter += ballSpeed;
+    }
+
+}
---
+++ Tusi motion/Tusi motion/Assets/Script/TusiBallManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TusiBallManager : MonoBehaviour
+{
+    public Object prefab;
+    List<GameObject> balls = new List<GameObject>();
+    float newAngle = 0f;
+    float shootingSpeed = 6f;
+    public GameObject parent;
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ballCreate(newAngle);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && balls.Count > 0)
+        {
+            shootBall();
+
+        }
+    }
+    private void ballCreate(float angle)
+    {
+        GameObject newGameObject = Instantiate(prefab) as GameObject;
+        //newGameObject.transform.SetParent(transform);
+        BallMovement ballmovement = newGameObject.GetComponent<BallMovement>();
+        ballmovement.target = gameObject;
+        ballmovement.projectLineAngle = angle;
+        if (balls.Count == 0)
+        {
+            ballmovement.counter = angle;
+        }
+        else
+        {
+            BallMovement lastBallmovement = balls[balls.Count - 1].GetComponent<BallMovement>();
+            ballmovement.counter = lastBallmovement.counter - 0.01f;
+        }
+        balls.Add(newGameObject);
+        newAngle += 0.5f;
+    }
+    private void shootBall()
+    {
+        GameObject shootingBall = balls[balls.Count - 1];
+        shootingBall.GetComponent<BallMovement>().isShooting = true;
+        shootingBall.GetComponent<BallMovement>().ShootDirection = transform.forward.normalized;
+        balls.RemoveAt(balls.Count - 1);
+    }
+}
